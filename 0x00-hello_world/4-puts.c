4e1a734b9a063b74800b3a827695f3ed5343712c
preetyjossy <bensonjosephine36@.com>
2022-09-08 21:48:13 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,12 @@
  *  Return: 0 if success
  */
 
- int main(void)
+int main(void)
 
- {
+{
 
-	 puts("\"programming is like building a multilingual puzzle");
+	puts("programming is like building a multilingual puzzle");
 
-	 return (0);
+	return (0);
 
- }
+}
