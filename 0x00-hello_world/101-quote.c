9d2d42ab45ac39a1905e6d3cedee9758a60dd242
preetyjossy <bensonjosephine36@.com>
2022-09-21 09:05:10 -0700

    0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- *main -Entry point
+ * main - Entry point
  *
- * Return:Always 1
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"59);
-	return(1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
